2a07463dcb5d4c93a60b85688ac6c08c529c82aa
k-jaehyun <wogus146@gmail.com>
2024-05-24 16:33:21 +0900

    fix: JwtAuthorizationFilter 가독성 개선

--- src/main/java/com/example/jujuassembly/global/filter/JwtAuthorizationFilter.java
+++ src/main/java/com/example/jujuassembly/global/filter/JwtAuthorizationFilter.java
@@ -39,8 +39,6 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
 
       UserDetailsImpl userDetails;
 
-      String accessTokenValue = accessToken.substring(7);
-
       // accessToken이 만료되었는지 확인
       if (jwtUtil.shouldAccessTokenBeRefreshed(accessToken.substring(7))) {
         String refreshTokenValue = jwtUtil.getRefreshtokenByAccessToken(accessToken).substring(7);
@@ -55,10 +53,12 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
           jwtUtil.regenerateToken(newAccessToken, accessToken, refreshTokenValue);
 
           // 재발급된 토큰으로 검증 진행하도록 대입
-          accessTokenValue = newAccessToken.substring(7);
+          accessToken = newAccessToken;
         }
-        // 유효하지 않다면 재발급 없이 만료된 상태로 진행
+        // refreshToken이 유효하지 않다면 재발급 없이 만료된 상태로 진행
       }
+
+      String accessTokenValue = accessToken.substring(7);
 
       if (jwtUtil.validateToken(accessTokenValue)) {
         Claims info = jwtUtil.getUserInfoFromToken(accessTokenValue);
